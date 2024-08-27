e114618f208b3d5d615f0fb60c05abf6fd883447
Thomas Bouldin <inlined@google.com>
2024-08-27 10:36:02 -0700

    Add debug route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const path = require("path");
 
 const app = express();
 
+app.get("debug", (req, res) => {
+  console.log("Headers are", req.rawHeaders);
+  res.send("OK");
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || 3000;
